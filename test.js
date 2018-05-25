1d71cbd542f4e769548e3e5601c758b775f51cb3
xiaoqiang0723 <457954719@qq.com>
2018-05-25 09:39:47 +0800

    修改错误

--- test.js
+++ test.js
@@ -5,7 +5,8 @@ const request = require('request')
 describe('/start接口测试', () => {
 	it('/start fun start', (done) => {
 		request.post('http://localhost:3000/start', (error, response, body) => {
-			(`${error}`).should.be.equalOneOf(['undefined', 'null'])
+			// eslint-disable-next-line
+			should(error).not.be.ok()
 			response.statusCode.should.be.equal(200)
 			body.should.be.equal('OK')
 			done()
@@ -16,7 +17,8 @@ describe('/start接口测试', () => {
 describe('/:number接口测试', () => {
 	it('/:number fun start', (done) => {
 		request.get('http://localhost:3000/20', (error, response, body) => {
-			(`${error}`).should.be.equalOneOf(['undefined', 'null'])
+			// eslint-disable-next-line
+			should(error).not.be.ok()
 			response.statusCode.should.be.equal(200)
 			body.should.be.equalOneOf(['bigger', 'smaller', 'equal'])
 			done()
@@ -26,15 +28,17 @@ describe('/:number接口测试', () => {
 
 function gameTest(i, callback) {
 	request(`http://localhost:3000/${i}`, (error, response, body) => {
-		console.log(i)
-		console.log(body);
-		(`${error}`).should.be.equalOneOf(['undefined', 'null'])
+		// console.log(i)
+		// console.log(body)
+		// eslint-disable-next-line
+		should(error).not.be.ok()
+		// (`${error}`).should.be.equalOneOf(['undefined', 'null'])
 		response.statusCode.should.be.equal(200)
 		body.should.match((n) => {
 			if (n === 'equal') {
 				callback()
 			} else {
-				gameTest(i + 1)
+				gameTest(i + 1, callback)
 			}
 		})
 	})
@@ -44,14 +48,15 @@ function gameTest(i, callback) {
 describe('/完整测试', () => {
 	beforeEach(done =>
 		request.post('http://localhost:3000/start', (error, response, body) => {
-			(`${error}`).should.be.equalOneOf(['undefined', 'null'])
+			// eslint-disable-next-line
+			should(error).not.be.ok()
 			response.statusCode.should.be.equal(200)
 			body.should.be.equal('OK')
 			done()
 		}))
 	describe('/猜数开始', () => {
 		it('/guess number start', (done) => {
-			gameTest(0, done())
+			gameTest(0, done)
 		})
 	})
 })
